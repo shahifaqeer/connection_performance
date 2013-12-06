279e895d7920692bfaba1409546cfe893e4f5404
Sarthak Grover <sgrover@gatech.edu>
2013-12-06 18:50:07 -0500

    fix constants - port numbers

--- src/constants.py
+++ src/constants.py
@@ -1,32 +1,32 @@
 time_sleep = 10.0 #seconds
 
-#router S options
+#server S options
 S_user = 'gnoise'
 S_pass = 'gtnoise'
 S_ip = '192.168.20.1' #'130.207.97.74'
-S_port = 5001
-
+S_port = 5005
+S_udp = 6005
 #router R options
 R_user = 'root'
 R_pass = 'passw0rd'
 R_ip = '192.168.10.1'
-R_port = 5002
-
-# A : hnl1
-A_user = 'gtnoise'
-A_pass = 'gtnoise'
-A_ip = '192.168.10.184'
-A_port = 5003
-
+R_port = 5004
+R_udp = 6004
+# C : hnl-nuc1
+C_user = 'gtnoise'
+C_pass = 'gtnoise'
+C_ip = '192.168.10.158'
+C_port = 5003
+C_udp = 6003
 # B : hnl2
 B_user = 'gtnoise'
 B_pass = 'gtnoise'
 B_ip = '192.168.10.158'
-B_port = 5004
-
-# B : hnl2
-C_user = 'gtnoise'
-C_pass = 'gtnoise'
-C_ip = '192.168.10.158'
-C_port = 5005
-
+B_port = 5002
+B_udp = 6002
+# A : hnl1
+A_user = 'gtnoise'
+A_pass = 'gtnoise'
+A_ip = '192.168.10.184' #192.168.10.149
+A_port = 5001
+A_udp = 6001
