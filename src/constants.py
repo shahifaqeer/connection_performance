755dae51a348ca92c5e1313f53f8a37a57a024a9
Sarthak Grover <shahifaqeer@gmail.com>
2013-12-06 18:15:05 -0500

    con:q!

--- src/constants.py
+++ src/constants.py
@@ -1,23 +1,24 @@
 time_sleep = 10.0 #seconds
 
-A_ip = '192.168.142.184'
+A_ip = '192.168.10.184'
 #A_ip = '127.0.0.1'
 A_port = 5006
 
 #router R options
-R_ip = '192.168.142.1'
+R_ip = '192.168.10.1'
 R_port = 5004
-R_pass = 'h0m3n3t' #'bismark123'
+R_pass = 'passw0rd' #'bismark123'
 R_key_string = 'AAAAB3NzaC1yc2EAAAADAQABAAAAgnH5yk05kTGEaPMDxr61H76uO8TkGKsBZcJBGdip8idCi3Dqaf1Hjr7RDIWCxyr8G8TBeVDSe53kwDqWIz1oa7niVtyTts66/Fbj4NeEEy4wzyNm2M1GgA1+yGsfo9WMtiuPuuAqL+ZFGO9qrcSovk+qpzp2l0THUs+5jf6/6GA5aF0='
 
 #client2 B options
 B_user = 'gtnoise'
 B_pass = 'gtnoise'
-B_ip = '192.168.142.158'
+B_ip = '192.168.10.158'
 #B_ip = '127.0.0.1'
 B_port = 5005
 
 #server S options
 S_user = 'gtnoise'
 S_pass = 'gtnoise'
-S_ip = '192.168.1.1'
+S_ip = '192.168.20.1'
+S_port = '5007'
