a93d829b3e6a6835419ca5a72842b0413f9c1426
Sarthak Grover <sgrover@gatech.edu>
2013-12-08 19:53:17 -0500

    make oops model for each host

---
+++ src/mytests.py
@@ -0,0 +1,8 @@
+from utils import RemoteHost
+from constants import *
+
+A = RemoteHost(A_ip, A_user, A_pass, A_port, A_udp)
+B = RemoteHost(B_ip, B_user, B_pass, B_port, B_udp)
+C = RemoteHost(C_ip, C_user, C_pass, C_port, C_udp)
+R = RemoteHost(R_ip, R_user, R_pass, R_port, R_udp)
+S = RemoteHost(S_ip, S_user, S_pass, S_port, S_udp)
--- src/utils.py
+++ src/utils.py
@@ -6,36 +6,65 @@ import time
 # local server ssh parameters to authenticate
 # make sure all systems (A, B, S, R) have been ssh-ed into by
 # C at least once!
-def connectHost(ip, user, passwd):
-  host = paramiko.SSHClient()
-  host.load_system_host_keys()
-  host.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-  host.connect(ip, username=user, password=passwd)
-  return host
 
-def check_connect(host):
-  stdin, stdout, stderr = host.exec_command('ls /tmp/')
-  for line in stdout:
-    print 'check host: ' + line.strip('\n')
-  return
+class RemoteHost:
+  def __init__(self, ip, user, passwd, tcp_port, udp_port, name):
+    # name can only be 'A', 'B', 'C', 'R', or 'S'
+    self.name = name
+    self.ip = ip
+    self.user = user
+    self.passwd = passwd
+    self.tcp_port = tcp_port
+    self.udp_port = udp_port
 
-def remoteCommand(host, cmd, logfilename):
-  """This should be used for starting iperf servers, pings,
-  tcpdumps, etc.
+    self.host = self.connectHost(ip, user, passwd)
 
-  Commands issued here can be killed by cmd = 'killall <cmd>'"""
-  if logfilename:
-    cmd = cmd + ' >> testlogs/' + logfilename + ' &'
-  stdin, stdout, stderr = host.exec_command(cmd)
-  logcmd(cmd)
-  return
+  def connectHost(self, ip, user, passwd):
+    host = paramiko.SSHClient()
+    host.load_system_host_keys()
+    host.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    host.connect(ip, username=user, password=passwd)
+    return host
+
+  def check_connect(self):
+    stdin, stdout, stderr = self.host.exec_command('ls /tmp/')
+    for line in stdout:
+      print 'check host: ' + line.strip('\n')
+    return
+
+  def remoteCommand(self, cmd, logfilename):
+    """This should be used for starting iperf servers, pings,
+    tcpdumps, etc.
+
+    Commands issued here can be killed by cmd = 'killall <cmd>'"""
+    if logfilename:
+      cmd = cmd + ' >> testlogs/' + logfilename + ' &'
+    stdin, stdout, stderr = self.host.exec_command(cmd)
+    self.logcmd(cmd)
+    return
+
+  def startPingAll(self):
+    self.remoteCommand('sh Browserlab/pings/pingall.sh &')
+    return
+
+  def stopPingAll(self):
+    self.remoteCommand('killall ping')
+    # self.remoteLogTransfer()
+
+  def startIperfServer(self):
+    if self.name == 'S':
+      self.remoteCommand('ipef -s -p ')
 
 # specific functions
-def createDataLogDir(server):
-  """a server function only"""
-  foldername = str(time.localtimealtime()[0])+str(time.localtime()[1])+str(time.localtime()[2])
-  remoteCommand(server, 'mkdir -p /home/gtnoise/data/'+foldername)
-  return
+  def createDataLogDir(self):
+    """a server function only"""
+    if self.name == 'S':
+      foldername = str(time.localtimealtime()[0])+str(time.localtime()[1])+str(time.localtime()[2])
+      self.remoteCommand('mkdir -p /home/gtnoise/data/'+foldername)
+    return
 
-def remoteLogTransfer(host, logfilename):
-  """Transfer all logfiles to homenetworklab/data/<DATE>/"""
+  def remoteLogTransfer(self, srclog, dstdir):
+    """Transfer all logfiles to homenetworklab/data/<DATE>/"""
+    # TODO
+    pass
+    return
